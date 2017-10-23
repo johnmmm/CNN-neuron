2e4d89f34c7c206bb36b7662dafb9160be9a30fa
周京汉 <zhoujh15@mails.tsinghua.edu.cn>
2017-10-23 15:24:52 +0800

    a running version(still have a bug)

--- functions.py
+++ functions.py
@@ -23,27 +23,13 @@ def conv2d_forward(input, W, b, kernel_size, pad):
     w_out = w_in + 2 * pad - kernel_size + 1
     
     input_pad = np.lib.pad(input, ((0,0), (0,0), (pad,pad), (pad,pad)), mode='constant', constant_values=0)
-    #input_pad = input_pad.transpose(1,0,2,3)
-
-    # new_b = np.zeros(c_out*h_out*w_out).reshape(c_out, h_out, w_out)
-    # for i in range(0, c_out):
-    #     new_b[c_out, :, :] = np.full((h_out, w_out), b[i]) 
 
     output = np.zeros(batch*c_out*h_out*w_out).reshape(batch, c_out, h_out, w_out)
-    #output = np.random.randn(batch, c_out, h_out, w_out)
     for i in range(0, batch):
-        #output[i, :, :, :] += new_b
         for j in range(0, c_out):
             for k in range(0, c_in):
                 output[i, j, :, :] += scipy.signal.convolve2d(input_pad[i, k, :, :], np.rot90(W[j, k, :, :], 2), mode='valid')+b[j]
-    
-    # output = np.random.randn(c_out, batch, h_out, w_out)
-    # W_3 = W.reshape(c_out, c_in, 1, kernel_size, kernel_size)
 
-    # for j in range(0, c_out):
-    #     for k in range(0, c_in):
-    #         output[j, :, :, :] += scipy.signal.fftconvolve(input_pad[k, :, :, :], np.rot90(W_3[j, k, :, :, :], 2), mode='valid') + b[j]
-    # output = output.transpose(1,0,2,3)
     return output
 
 def conv2d_backward(input, grad_output, W, b, kernel_size, pad):
@@ -65,58 +51,27 @@ def conv2d_backward(input, grad_output, W, b, kernel_size, pad):
     batch, c_in, h_in, w_in = input.shape
     _, c_out, h_out, w_out = grad_output.shape
 
-    #3
-    #grad_input = np.zeros(batch*c_in*h_in*w_in).reshape(c_in, batch, h_in, w_in)
-    #grad_W = np.zeros(c_out*c_in*kernel_size*kernel_size).reshape(c_out, c_in, kernel_size, kernel_size)
-
-    #2
     grad_input = np.zeros(batch*c_in*h_in*w_in*c_out).reshape(batch, c_in, c_out, h_in, w_in)
     grad_W = np.zeros(c_out*c_in*kernel_size*kernel_size*batch).reshape(c_out, c_in, batch, kernel_size, kernel_size)
-    
     grad_b = np.zeros(c_out)
-
+    
     grad_output = grad_output[:, :, 1:h_out-1, 1:w_out-1]
 
-    # now = datetime.now()
-    # print(now)
     for i in range(0, batch):
         for j in range(0, c_in):
             for k in range(0, c_out):
                 grad_input[i, j, :, :] = scipy.signal.convolve2d(grad_output[i, k, :, :], W[k, j, :, :], mode='full')
                 # temp = scipy.signal.convolve2d(grad_output[i, k, :, :], W[k, j, :, :], mode='full')
                 # grad_input[i, j, k, :, :] = temp[pad:h_in+pad, pad:w_in+pad]
+                #need to change!
     grad_input = grad_input.sum(axis=2)
-    # now = datetime.now()
-    # print(now)
+
     for i in range(0, batch):
         for j in range(0, c_in):
             for k in range(0, c_out):  
                 grad_W[k, j, i, :, :] = scipy.signal.convolve2d(input[i, j, :, :], np.rot90(grad_output[i, k, :, :], 2), mode='valid')
     grad_W = grad_W.sum(axis=2)
     grad_b = np.sum(grad_output, axis=(0,2,3)) #ok
-    #now = datetime.now()
-    # print(now)
-    # print('~~~~~~~~~~~~~~~~~')
-    
-    # now = datetime.now()
-    # print(now)
-    # W_3 = W.reshape(c_out, c_in, 1, kernel_size, kernel_size)
-    #grad_W_3 = grad_W.reshape(c_out, c_in, 1, kernel_size, kernel_size)#xiamian
-    # input = input.transpose(1,0,2,3)
-    # grad_output = grad_output.transpose(1,0,2,3)
-    # for j in range(0, c_in):
-    #     for k in range(0, c_out):
-    #         grad_input[j, :, :, :] += scipy.signal.fftconvolve(grad_output[k, :, :, :], W_3[k, j, :, :, :], mode='full')
-    # grad_input = grad_input.transpose(1,0,2,3)
-    # now = datetime.now()
-    # print(now)
-    # for j in range(0, c_in):
-    #     for k in range(0, c_out):    
-    #         grad_W_3[k, j, :, :, :] = scipy.signal.fftconvolve(input[j, :, :, :], np.rot90(grad_output[k, :, :, :], 2), mode='valid')
-    # grad_W = grad_W_3.reshape(c_out, c_in, kernel_size, kernel_size)
-    # now = datetime.now()
-    # print(now)
-    #print("~~~~~~~~~~~~~~~~~~~")
 
     return grad_input, grad_W, grad_b
 
@@ -136,11 +91,6 @@ def avgpool2d_forward(input, kernel_size, pad):
     batch, c_in, h_in, w_in = input.shape
     h_out = (int)((h_in + 2 * pad) / kernel_size)
     w_out = (int)((w_in + 2 * pad) / kernel_size)
-    # h_pool = (int)((h_in + 2 * pad)/h_out)
-    # w_pool = (int)((w_in + 2 * pad)/w_out)
-
-    # output = np.random.randn(batch, c_in, h_out, w_out)
-    # temp = np.random.randn(h_pool, w_pool)
 
     input_tem = input.reshape(batch, c_in, h_out, kernel_size, w_out, kernel_size)
     output = np.mean(input_tem, axis=(3,5))
@@ -163,8 +113,7 @@ def avgpool2d_backward(input, grad_output, kernel_size, pad):
     batch, c_in, h_in, w_in = input.shape
     h_out = (h_in + 2 * pad) / kernel_size
     w_out = (w_in + 2 * pad) / kernel_size
-    # h_pool = (h_in + 2 * pad)/h_out
-    # w_pool = (w_in + 2 * pad)/w_out
+
     poolingsize = kernel_size**2
     temp = grad_output.repeat(kernel_size, axis=2).repeat(kernel_size, axis=3)
     temp = temp / poolingsize
